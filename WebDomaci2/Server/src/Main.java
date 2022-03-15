af8c6af7bef6836f9a92a04ab41a712cd010661d
Filip-Papic <filippapic97@gmail.com>
2022-03-15 17:22:18 +0100

    finito

--- WebDomaci2/Server/src/Main.java
+++ WebDomaci2/Server/src/Main.java
@@ -24,36 +24,3 @@ public class Main {
         }
     }
 }
-
-/*
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.*;
-
-public class Main {
-
-    public static final int PORT = 9001;
-    public static List<ServerThread> clientList = new ArrayList<>();
-
-    public static void main(String[] args) {
-        try {
-            ServerSocket serverSocket = new ServerSocket(Main.PORT);
-            ServerThread st = null;
-            while (true) {
-                Socket socket = serverSocket.accept();
-                System.out.println("NEW CONNECTION");
-
-                st = new ServerThread(socket);
-                clientList.add(st);
-                Thread serverThread = new Thread(st);
-                serverThread.start();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-}
-*/
